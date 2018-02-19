6569239a88815b4e401bd8598b71261295643a59
Angelo <angeloraso6@gmail.com>
2018-02-19 00:00:22 -0300

    El script ahora muestra información relacionada a las materias electivas

--- parser.js
+++ parser.js
@@ -1,17 +1,84 @@
 var Numeros = {
-      "uno" : 1, "dos": 2, "tres": 3, "cuatro": 4, "cinco":5, "seis":6, "siete":7, "ocho":8, "nueve":9, "diez":10, "Ausen.":0, "Insc.":0, "Aprob.":0,
+      "uno" : 1, 
+      "dos": 2, 
+      "tres": 3, 
+      "cuatro": 4, 
+      "cinco":5, 
+      "seis":6, 
+      "siete":7, 
+      "ocho":8, 
+      "nueve":9, 
+      "diez":10, 
+      "Ausen.":0, 
+      "Insc.":0, 
+      "Aprob.":0,
+}
+var electivasArray = [
+  "Seguridad en Sistemas de Información(Elec.)",
+  "Tecnología y Gestión Web(Elec.)",
+  "Comunicación Profesional(Elec.)",
+  "Responsabilidad Social Institucional(Elec.)",
+  "Emprendedorismo(Elec.)",
+  "Sistemas de transmisión inalámbrica(Elec.)",
+  "Actualización Tecnológica e Innovación(Elec.)",
+  "Auditoría en Sistemas de Información(Elec.)",
+  "Bioingeniería(Elec.)",
+  "Tecnologías para la Explotación de la Información(Elec.)",
+  "Metodología de la Investigación(Elec.)",
+  "Aplicaciones Móviles(Elec.)",
+  "Metodología Agiles (Elec.)",
+  "Administración de Base de Datos(Elec.)",
+  "Sistemas Operativos Avanzados(Elec.)",
+  "Aplicaciones en Tiempo Real(Elec.)",
+  "Interacción Hombre-Sistemas(Elec.)",
+  "Internetworking(Elec.)",
+  "Modelos de Calidad en la Industria del Software(Elec.)",
+  "Protocolos y Seguridad en redes inalámbricas(Elec.)",
+  "Tecnología de Información para la Gestión Empresarial(Elec.)",
+  "Ingeniería en Calidad(Elec.)",
+];
+
+var electivasObjeto = {
+  "Seguridad en Sistemas de Información(Elec.)": 2,
+  "Tecnología y Gestión Web(Elec.)": 2,
+  "Comunicación Profesional(Elec.)": 3,
+  "Responsabilidad Social Institucional(Elec.)": 2,
+  "Emprendedorismo(Elec.)": 2,
+  "Sistemas de transmisión inalámbrica(Elec.)": 2,
+  "Actualización Tecnológica e Innovación(Elec.)": 3,
+  "Auditoría en Sistemas de Información(Elec.)": 2,
+  "Bioingeniería(Elec.)": 2,
+  "Tecnologías para la Explotación de la Información(Elec.)": 2,
+  "Metodología de la Investigación(Elec.)": 2,
+  "Aplicaciones Móviles(Elec.)": 3,
+  "Metodología Agiles (Elec.)": 3,
+  "Administración de Base de Datos(Elec.)": 2,
+  "Sistemas Operativos Avanzados(Elec.)": 2,
+  "Aplicaciones en Tiempo Real(Elec.)": 5,
+  "Interacción Hombre-Sistemas(Elec.)": 2,
+  "Internetworking(Elec.)": 4,
+  "Modelos de Calidad en la Industria del Software(Elec.)": 2,
+  "Protocolos y Seguridad en redes inalámbricas(Elec.)": 2,
+  "Tecnología de Información para la Gestión Empresarial(Elec.)": 2,
+  "Ingeniería en Calidad(Elec.)": 3,
 }
 
 var cantidadMateriasFaltantes;
 var numeroHTML;
 var nota;
 var notaTexto;
-// Este número depende de las electivas que se elijan. Pueden ser más.
+// Este número depende de las electivasObjeto que se elijan. Pueden ser más.
 var cantidadMateriasTotalesCarreraSistemas = 43;
 var sumaTotal = 0;
 var sumaSinAplazos = 0;
 var cantidadNotasConAplazos = 0;
 var cantidadNotasSinAplazos = 0;
+
+var electivaHTML;
+var electivaTexto;
+var cantidadElectivasCursadas = 0;
+var cantidadHorasElectivas = 0;
+var cantidadHorasElectivasNecesarias = 22;
 
 var elementos = document.getElementsByClassName("textoTabla")
 for ( var i = 1; i < elementos.length; i++ ) {
@@ -29,12 +96,35 @@ for ( var i = 1; i < elementos.length; i++ ) {
       cantidadNotasConAplazos += 1;
     }
   }
+
+  // Se guarda la materia en la variable electivaHTML
+  electivaHTML = elementos[i].getElementsByTagName("td")[1];
+  if ( electivaHTML !== undefined ) {
+    // Se convierte a texto el nombre de la materia
+    electivaTexto = electivaHTML.innerText;
+
+    // Se controla que la materia obtenida sea una Electiva
+    for(var j=0; j < electivasArray.length; j++){
+
+      if( electivaTexto === electivasArray[j]){
+      
+        horasElectiva = electivasObjeto[electivaTexto]; // Se obtienen las horas de la respectiva Electiva
+        cantidadHorasElectivas += horasElectiva; // Se suman dichas horas al total
+        cantidadElectivasCursadas += 1; // Se suma una electiva al total de electivas cursadas
+      }
+    }
+  }
 }
 
 cantidadMateriasFaltantes = cantidadMateriasTotalesCarreraSistemas - cantidadNotasSinAplazos;
 
-console.log("Promedio con aplazos: " + (sumaTotal/cantidadNotasConAplazos).toFixed(2));
-console.log("Promedio sin aplazos: " + (sumaSinAplazos/cantidadNotasSinAplazos).toFixed(2));
-console.log("Cantidad de materias aprobadas: " + cantidadNotasSinAplazos);
-console.log("Cantidad de materias faltantes: " + cantidadMateriasFaltantes);
-console.log("Porcentaje de materias aprobadas: " + (cantidadNotasSinAplazos*100/cantidadMateriasTotalesCarreraSistemas).toFixed(2) + "%");
+console.log("Promedio Con Aplazos: " + (sumaTotal/cantidadNotasConAplazos).toFixed(2));
+console.log("Promedio Sin Aplazos: " + (sumaSinAplazos/cantidadNotasSinAplazos).toFixed(2));
+console.log("Cantidad de Materias Aprobadas: " + cantidadNotasSinAplazos);
+console.log("Cantidad de Materias Faltantes: " + cantidadMateriasFaltantes);
+console.log("Porcentaje de Materias Aprobadas: " + (cantidadNotasSinAplazos*100/cantidadMateriasTotalesCarreraSistemas).toFixed(2) + "%");
+
+console.log("Cantidad de Electivas Cursadas: " + cantidadElectivasCursadas);
+console.log("Cantidad de Horas de Electivas: " + cantidadHorasElectivas);
+console.log("Porcentaje del Total de Materias Electivas: " + (cantidadHorasElectivas*100/cantidadHorasElectivasNecesarias).toFixed(2) + "%");
+console.log("Cantidad de Horas de Electivas Faltantes: " + (cantidadHorasElectivasNecesarias - cantidadHorasElectivas));
